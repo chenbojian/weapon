ca7b4681a67a63d4f96fa1d052334c884c673066
chenbojian <chenbojian@foxmail.com>
2015-08-06 19:04:02 +0800

    add test

--- src/test/java/com/cbj/weapon/PlayerTest.java
+++ src/test/java/com/cbj/weapon/PlayerTest.java
@@ -1,9 +1,16 @@
 package com.cbj.weapon;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
-public class PlayerTest extends TestCase {
+public class PlayerTest {
+    @Before
     public void setUp() {
+
+    }
+
+    @Test
+    public void should_show_profession_and_name_when_attack() {
 
     }
 

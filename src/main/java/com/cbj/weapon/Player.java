5d78c0c73e97cebe5463750096cf78254be76470
chenbojian <chenbojian@foxmail.com>
2015-08-06 14:53:40 +0800

    Step2, can show attack information. fix a bug when judge isDead.

--- src/main/java/com/cbj/weapon/Player.java
+++ src/main/java/com/cbj/weapon/Player.java
@@ -4,6 +4,7 @@ public class Player {
     private int lifeValue;
     private String name;
     private int attackValue;
+    private int damagedValue;
 
     public Player(String name, int lifeValue, int attackValue) {
         this.lifeValue = lifeValue;
@@ -13,14 +14,18 @@ public class Player {
 
     public void attack(Player damagedPlayer) {
         damagedPlayer.damaged(this.getAttackValue());
+        System.out.println(this.getName() + "攻击了" + damagedPlayer.getName() + "," +
+                damagedPlayer.getName() + "受到了" + damagedPlayer.getDamagedValue() + "点伤害," +
+                damagedPlayer.getName() + "剩余生命:" + damagedPlayer.getLifeValue());
     }
 
     private void damaged(int attackValue) {
+        this.damagedValue = attackValue;
         this.lifeValue -= attackValue;
     }
 
     public boolean isDead() {
-        return this.lifeValue > 0;
+        return this.lifeValue <= 0;
     }
 
 
@@ -39,5 +44,13 @@ public class Player {
 
     public void setAttackValue(int attackValue) {
         this.attackValue = attackValue;
+    }
+
+    public int getDamagedValue() {
+        return damagedValue;
+    }
+
+    public void setDamagedValue(int damagedValue) {
+        this.damagedValue = damagedValue;
     }
 }
